8462487d5f15aece4dee554b7504c11ce7258a99
prasadambati1 <ambati.prasad@yahoo.com>
2023-07-23 23:59:38 +0530

    chenges done in master

--- DBUtils.java
+++ DBUtils.java
@@ -6,3 +6,7 @@ hello the file contains DBU information
 hogya hogya bdi mhnt pura din lga hogya 
 
 this is addong from the decelopment branch
+
+
+this is adding in master branch
+want to test merge 
